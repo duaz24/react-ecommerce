ff581421e2aaac2da1f1d2495d50d3349a90bca1
Duarte Azevedo <A047329@ipmaia.pt>
2026-01-12 19:43:44 +0000

    Add Footer component and restructure layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 import { CartProvider } from "./context/CartContext";
 import { initScrollAnimations } from "./scrollAnimations";
 
@@ -15,12 +17,24 @@ export default function App() {
   return (
     <CartProvider>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+        {/* Flex column para empurrar o footer para baixo se a página for pequena */}
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+          
+          <Navbar />
+          
+          {/* Main content que cresce para ocupar espaço disponível */}
+          <div style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+          </div>
+
+          <Footer />
+          
+        </div>
       </BrowserRouter>
     </CartProvider>
   );
