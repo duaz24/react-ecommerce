b5cd7a1e58444d902a331725e6518ae06d1faebe
Duarte Azevedo <A047329@ipmaia.pt>
2026-01-12 20:16:46 +0000

    Add Shipping component for delivery information

---
+++ src/pages/Shipping.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Shipping() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Envios e Entregas</h2>
+      <p>Aqui ficam as informações sobre os métodos de envio...</p>
+    </div>
+  );
+}
