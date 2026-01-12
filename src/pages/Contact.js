24ea0eb94f294112508340e5d663ed81c006c411
Duarte Azevedo <A047329@ipmaia.pt>
2026-01-12 20:18:14 +0000

    Add Contact component with contact information

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Contacta-nos</h2>
+      <p>Entra em contacto connosco através do email...</p>
+    </div>
+  );
+}
